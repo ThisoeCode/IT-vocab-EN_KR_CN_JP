ccc6ce7c295c630018e2f2e8039c54f0b1b163cc
Thisoe <thisoecode@gmail.com>
2024-04-07 02:05:48 +0900

    TEST APP DONE

---
+++ app/api/v0/route.js
@@ -0,0 +1,18 @@
+import{NextResponse}from"next/server"
+import insu from"@/lib/_insu"
+import uuid from"@/lib/uuid"
+
+export async function PUT(req){
+  req = await req.json()
+  const res = await(
+    await(async _=>{
+      const {db} = await insu()
+      return db.collection(process.env.DB_COLL)
+    })()
+  ).insertOne(req)
+  console.log(`[Thisoe msg::PUT 201] Added new row: ObjectId[${res.insertedId}]`)
+  return NextResponse.json(
+    {status:"success",uuid:uuid.shorten(req.id)},
+    {status:201},
+  )
+}
